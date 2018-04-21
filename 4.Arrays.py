eede8cd9781a144b3d32ea37ddec7751dbb75bd6
faffyroypeter <faffyroypeter@gmail.com>
2018-04-21 16:46:19 +0530

    Add files via upload

---
+++ 4.Arrays.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+myArray = [1,2,3,4]
+
+type(myArray)
+
+# not supported as it is a list
+print(myArray ** 2 )
+
+arr1 = np.array(myArray)
+print(arr1 * 2)
+print(arr1 ** 2)
+print(arr1 ** 2 + arr1)
+
+print(arr1 > 2)
+print(arr1[arr1 > 2])
+
+arr1.max()
+arr1.mean()
+arr1.min()
+
+# Matrix based operations
+
+matrix1 = np.array([[1,2],[3,4],[3,5]])
+print (matrix1 * 1)
+
+# Item wise addition
+print (matrix1 + 1)
+
+# Element wise multiplication
+print (matrix1 * 10)
+matrix2 = np.array([[1,2,3],[3,4,5]])
+print (matrix1 * matrix2)
+
+# Matrix Multiplication
+matrix1.dot(matrix2)
+
+matrix1.reshape((2,4))
